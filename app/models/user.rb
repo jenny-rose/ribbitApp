cc089900a823c24420e9519cce37b6ae2c86efb3
Jenny Coffman <scarling@gmail.com>
2013-07-26 18:37:14 -0500

    User form and profile pages

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,14 @@
 class User < ActiveRecord::Base
-    attr_accessible :avatar_url, :email, :name, :password, :password_confirmation,
+    before_validation :prep_email
+    before_save :create_avatar_url
+
+    attr_accessible :avatar_url, :email, :name, :password, :password_confirmation, :username
 
     has_secure_password
-    
-    before_validation: prep_email
-
-    before_save: create_avatar_url
 
     validates :name, presence: true
     validates :username, uniqueness: true, presence: true
-    validates :email, uniqueness: true, presence: true, format: { with: /^[\w.+-]+@
+    validates :email, uniqueness: true, presence: true, format: { with: /\A[\w.+-]+@([\w]+.)+\w+\z/ }
 
     private
 
@@ -18,6 +17,6 @@ class User < ActiveRecord::Base
     end
 
     def create_avatar_url
-    	self.avatar_url = "http://www.gravatar.com/avatar/#{Digest::MD5.hexidigest(self.email)}?s=50"
+    	self.avatar_url = "http://www.gravatar.com/avatar/#{Digest::MD5.hexdigest(self.email)}?s=50"
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 RibbitApp::Application.routes.draw do
   resources :users
+ 
+  root to: 'users#new'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
