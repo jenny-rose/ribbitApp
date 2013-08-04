aac92b22f2aa8e7a55b75c06ace7b493489d8158
Jenny Coffman <scarling@gmail.com>
2013-08-04 17:16:06 -0500

    Fixed some typos

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
-    has_many :follower_relationships, classname: "Relationship", foreign_key: "followed_id"
-    has_many :followed_relationships, classname: "Relationship", foreign_key: "follower_id"
+    has_many :follower_relationships, class_name:"Relationship", foreign_key: "followed_id"
+    has_many :followed_relationships, class_name: "Relationship", foreign_key: "follower_id"
     has_many :followers, through: :follower_relationships
     has_many :followed, through: :followed_relationships
     has_many :ribbits
